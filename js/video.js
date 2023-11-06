09850056dea10041ea5f5f0505b05bf55ad6f028
kysrit <102626439+kysrit@users.noreply.github.com>
2023-11-06 00:37:52 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var play = document.getElementById("play");
+	var slider = document.getElementById("slider");
+	var mute = document.getElementById("mute");
+	var slow = document.getElementById("slower");
+	var speed = document.getElementById("faster");
+	var info = document.getElementById("volume");
+	var pause = document.getElementById("pause");
+	var video = document.getElementById("player1");
+	var skip = document.getElementById("skip");
+	var oldbttn = document.getElementById("vintage");
+	var ogbttn = document.getElementById("orig");
+
+
+    video.autoplay = false;
+    video.loop = false;
+
+    play.addEventListener("click", function () {
+        video.play();
+        updateVolumeInfo();
+    });
+    oldbttn.addEventListener("click", function () {
+        video.classList.add("oldSchool");
+    });
+	
+    ogbttn.addEventListener("click", function () {
+        video.classList.remove("oldSchool");
+    });
+	slider.addEventListener("input", function () {
+        video.volume = slider.value / 100;
+        updateVolumeInfo();
+    });
+	skip.addEventListener("click", function () {
+        video.currentTime += 10;
+        if (video.duration < video.currentTime) {
+            video.currentTime = 0;
+        }
+        console.log(`Time: ${video.currentTime}`);
+    });
+	function updateVolumeInfo() {
+		info.textContent = `Volume: ${Math.round(video.volume * 100)}%`;
+	 }
+
+    pause.addEventListener("click", function () {
+        video.pause();
+    });
+
+	mute.addEventListener("click", function () {
+        video.muted = !video.muted;
+        if (!video.muted) {
+			mute.textContent = "Mute";
+        } else if(video.muted){
+            mute.textContent = "Unmute";
+        }
+    });
+
+    slow.addEventListener("click", function () {
+        video.playbackRate -= 1/10;
+        console.log(`Speed: ${video.playbackRate}`);
+    });
+
+    speed.addEventListener("click", function () {
+        video.playbackRate += 1/10;
+        console.log(`Speed: ${video.playbackRate}`);
+    });
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+});
